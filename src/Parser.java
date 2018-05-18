18ddb7d63c48e8a5b6c734ff65a4f01b13f5dfe4
dx07 <dx.msr07@gmail.com>
2018-05-18 10:37:37 +0200

    ok
    bite

--- src/Parser.java
+++ src/Parser.java
@@ -1,4 +1,52 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Parser
 {
+
+        public static void read(String FILENAME) {
+
+            BufferedReader br = null;
+            FileReader fr = null;
+
+            try {
+
+                fr = new FileReader(FILENAME);
+                br = new BufferedReader(fr);
+
+            } catch (IOException e) {
+
+                e.printStackTrace();
+
+            } finally {
+
+                try {
+
+                    if (br != null)
+                        br.close();
+
+                    if (fr != null)
+                        fr.close();
+
+                } catch (IOException ex) {
+
+                    ex.printStackTrace();
+
+                }
+
+            }
+
+        }
+
+        public static void main(String[] args){
+
+            String sCurrentLine;
+
+            while ((sCurrentLine = br.readLine()) != null) {
+                System.out.println(sCurrentLine);
+            }
+
+        }
 
 }
