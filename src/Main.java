38cb074c668cf320b6eafa2dee1b74d3dd23d0e8
dx07 <dx.msr07@gmail.com>
2018-06-03 19:14:31 +0200

    Best Main Ever

--- src/Main.java
+++ src/Main.java
@@ -4,15 +4,52 @@ public class Main
 {
 	public static void main(String[] args)
 	{
-		// testDirectory("./textJobData/Barnes");
-		// testFile(new File("./textJobData/test.txt"));
-		testFile(new File("./textJobData/Hurink_Data/sdata/la40.fjs"));
+		String path = null;
+		
+		//path = "./textJobData/Barnes";
+		//path = "./textJobData/test.txt";
+		path = "./textJobData/Hurink_Data/sdata/la40.fjs";
+		
+		try{
+			path = args[0];
+			System.out.println("Chemin de test passé en paramètre");
+		}
+		catch (Exception e){
+			System.out.println("Chemin de test automatique (voir code du main)");
+		}
+		
+		int res = test(path);
+		
+		if (res == 0)
+			System.out.println("Test complété !");
+		else
+			System.out.println("Erreur : Veuillez rentrer un chemin de fichier / dossier existant");
+			
+	}
+	
+	public static int test(String path){
+		
+		File path_to_test = new File(path);
+		
+		if (path_to_test.exists()) {
+		    if (path_to_test.isFile()) {
+		    	testFile(path_to_test);
+		    } else if (path_to_test.isDirectory()) {
+		        testDirectory(path_to_test);
+		    }
+		    else
+		    	return -1;   
+		}
+		else
+			return -1;
+		
+		return 0;
+		
 	}
 
-	public static void testDirectory(String path)
+	public static void testDirectory(File directory)
 	{
-		File folder = new File(path);
-		File[] files = folder.listFiles();
+		File[] files = directory.listFiles();
 		for (File file : files)
 			testFile(file);
 	}
